545dcbea79cf6d06210a68f212153b80b5032903
gamemasterplc <gamemasterplc@gmail.com>
2024-12-12 10:28:48 -0600

    Implement FizzBuzzifyAnInt in FizzBuzz class

--- FizzBuzzLib.Tests/FizzBuzzifyAnIntTests.cs
+++ FizzBuzzLib.Tests/FizzBuzzifyAnIntTests.cs
@@ -6,9 +6,18 @@ namespace FizzBuzzLib.Tests
     public class FizzBuzzifyAnIntTests
     {
         [Fact]
-        public void FizzBuzzifyAnInt_WhenPassedAndInt_ReturnsAString()
+        public void FizzBuzzifyAnInt_TestCases()
         {
-            // test here
+            //Create test class
+            var fizzBuzzClass = new FizzBuzz();
+            //Test neither multiple of 3 nor 5
+            Assert.Equal("13", fizzBuzzClass.FizzBuzzifyAnInt(13));
+            //Test multiple of 3
+            Assert.Equal("Fizz", fizzBuzzClass.FizzBuzzifyAnInt(6));
+            //Test multiple of 5
+            Assert.Equal("Buzz", fizzBuzzClass.FizzBuzzifyAnInt(10));
+            //Test multiple of 15
+            Assert.Equal("FizzBuzz", fizzBuzzClass.FizzBuzzifyAnInt(30));
         }
     }
 }
--- FizzBuzzLib/FizzBuzz.cs
+++ FizzBuzzLib/FizzBuzz.cs
@@ -8,5 +8,25 @@
 
             return "Hello World";
         }
+        public string FizzBuzzifyAnInt(int input)
+        {
+            string output = ""; //Start with blank output
+            //Add Fizz for multiple of 3
+            if (input % 3 == 0)
+            {
+                output += "Fizz";
+            }
+            //Add Buzz for multiple of 5
+            if (input % 5 == 0)
+            {
+                output += "Buzz";
+            }
+            //Replace cases that are neither with the input as a string
+            if (output == "")
+            {
+                output = input.ToString();
+            }
+            return output;
+        }
     }
 }
